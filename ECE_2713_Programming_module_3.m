7b183f473d4b904b8dadd2155a7daa88dc68ac8b
timmccoyrose <trose@ou.edu>
2017-03-27 18:13:55 -0500

    Add files via upload

---
+++ ECE_2713_Programming_module_3.m
@@ -0,0 +1,34 @@
+%Part A - Load file and set up arry 
+load('dt_file1.mat');
+
+
+
+
+
+
+
+
+%Part B - Second time array 
+
+
+
+
+
+%Part C - Idea Interpolation 
+
+
+
+
+
+
+
+%Part D - Sample and hold 
+
+
+
+
+
+
+
+
+%Part E - Plot Graphs
